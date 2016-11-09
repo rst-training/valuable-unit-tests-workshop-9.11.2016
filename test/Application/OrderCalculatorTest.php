11e95faa1092fe42dab4f7dc83f4849e368a55fe
Mateusz Badziński <mateusz.badzinski@rst.com.pl>
2016-11-09 12:12:18 +0100

    Refactor + uTest

--- test/Application/OrderCalculatorTest.php
+++ test/Application/OrderCalculatorTest.php
@@ -37,12 +37,7 @@ class OrderCalculatorTest extends \PHPUnit_Framework_TestCase
             ->method('calculateForSeat')
             ->willReturn(999);
 
-        $orderCalculator = $this->getMockBuilder(OrderCalculator::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $orderCalculator
-            ->method('getDiscountService')
-            ->willReturn($discountService);
+        $orderCalculator = new OrderCalculator($discountService);
 
         /** @var OrderCalculator $orderCalculator */
         $seats = new SeatsCollection();
@@ -53,5 +48,81 @@ class OrderCalculatorTest extends \PHPUnit_Framework_TestCase
         ];
 
         $this->assertEquals($price, $orderCalculator->calculateTotalCost($seats, $seatsPrice));
+    }
+
+    public function test_should_return_price_with_two_the_same_type_seat_in_order()
+    {
+        $price = 10;
+
+        $discountService = $this->getMockBuilder(DiscountService::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $discountService
+            ->method('calculateForSeat')
+            ->willReturn(999);
+
+        $orderCalculator = new OrderCalculator($discountService);
+
+        /** @var OrderCalculator $orderCalculator */
+        $seats = new SeatsCollection();
+        $seats->add(new Seat('catA', 2));
+
+        $seatsPrice = [
+            'catA' => [$price]
+        ];
+
+        $this->assertEquals($price * 2, $orderCalculator->calculateTotalCost($seats, $seatsPrice));
+    }
+
+    public function test_should_return_price_with_two_other_type_seat_in_order()
+    {
+        $priceA = 10;
+        $priceB = 50;
+
+        $discountService = $this->getMockBuilder(DiscountService::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $discountService
+            ->method('calculateForSeat')
+            ->willReturn(999);
+
+        $orderCalculator = new OrderCalculator($discountService);
+
+        /** @var OrderCalculator $orderCalculator */
+        $seats = new SeatsCollection();
+        $seats->add(new Seat('catA', 1));
+        $seats->add(new Seat('catB', 1));
+
+        $seatsPrice = [
+            'catA' => [$priceA],
+            'catB' => [$priceB],
+        ];
+
+        $this->assertEquals($priceA + $priceB, $orderCalculator->calculateTotalCost($seats, $seatsPrice));
+    }
+
+    public function test_should_return_price_with_one_discounter_seat_in_order()
+    {
+        $priceA = 10;
+        $priceDiscountedA = 5;
+
+        $discountService = $this->getMockBuilder(DiscountService::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $discountService
+            ->method('calculateForSeat')
+            ->willReturn($priceDiscountedA);
+
+        $orderCalculator = new OrderCalculator($discountService);
+
+        /** @var OrderCalculator $orderCalculator */
+        $seats = new SeatsCollection();
+        $seats->add(new Seat('catA', 1));
+
+        $seatsPrice = [
+            'catA' => [$priceA],
+        ];
+
+        $this->assertEquals($priceDiscountedA, $orderCalculator->calculateTotalCost($seats, $seatsPrice));
     }
 }
