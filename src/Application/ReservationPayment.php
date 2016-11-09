4da5e842a301ac2fddaacf12490de72609fa41fd
lhryniuk <lukasz.hryniuk@rst.com.pl>
2016-11-09 11:59:47 +0100

    Added ReservationPayment.php.

---
+++ src/Application/ReservationPayment.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace RstGroup\ConferenceSystem\Application;
+
+use RstGroup\ConferenceSystem\Domain\Payment\PaypalPayments;
+use RstGroup\ConferenceSystem\Domain\Reservation\ConferenceId;
+use RstGroup\ConferenceSystem\Domain\Payment\DiscountService;
+use RstGroup\ConferenceSystem\Domain\Reservation\OrderId;
+use RstGroup\ConferenceSystem\Domain\Reservation\ReservationId;
+use RstGroup\ConferenceSystem\Domain\Reservation\Seat;
+use RstGroup\ConferenceSystem\Domain\Reservation\SeatsCollection;
+use RstGroup\ConferenceSystem\Infrastructure\Reservation\ConferenceSeatsDao;
+use RstGroup\ConferenceSystem\Infrastructure\Reservation\ConferenceMemoryRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+class ReservationPayment
+{
+    public function __construct(DiscountService $discountService, ConferenceDao $conferenceDao)
+    {
+        $this->conferenceDao = $conferenceDao;
+        $this->discountService = $discountService;
+    }
+
+    public function getTotalCost(Reservation $reservation)
+    {
+        $totalCost = 0;
+        $seatsPrices = $this->conferenceDao->getSeatsPrices($conferenceId);
+        $seats = $reservation->getSeats();
+
+        foreach ($seats->getAll() as $seat) {
+            $priceForSeat = $seatsPrices[$seat->getType()][0];
+
+            $dicountedPrice = $this->DiscountService->calculateForSeat($seat, $priceForSeat);
+            $regularPrice = $priceForSeat * $seat->getQuantity();
+
+            $totalCost += min($dicountedPrice, $regularPrice);
+        }
+
+        return $totalCost;
+    }
+    private $
+}
